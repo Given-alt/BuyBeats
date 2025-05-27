2a0bcd410eb9df94fbd7270d9e6fa2b5d4c45a9e
Given <73137678+Given-alt@users.noreply.github.com>
2025-05-27 06:19:29 +0000

    Added beats display on homepage

--- flask_beatstore/app.py
+++ flask_beatstore/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,13 +16,40 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
+class Beat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    file_url = db.Column(db.String(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    beats = Beat.query.all()
+    return render_template('home.html', beats=beats)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+        
+        if existing_user:
+            flash('Username already exists!', 'danger')
+        else:
+            new_user = User(username=username, password=password)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created! You can now log in.', 'success')
+            return redirect(url_for('login'))
+    
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -43,24 +70,24 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+@app.route('/upload', methods=['GET', 'POST'])
+@login_required
+def upload():
+    if request.method == 'POST':
+        title = request.form['title']
+        price = float(request.form['price'])
+        file_url = request.form['file_url']
+        
+        new_beat = Beat(title=title, price=price, file_url=file_url, user_id=current_user.id)
+        db.session.add(new_beat)
+        db.session.commit()
+        
+        flash('Beat uploaded successfully!', 'success')
+        return redirect(url_for('home'))
+    
+    return render_template('upload.html')
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        existing_user = User.query.filter_by(username=username).first()
-        
-        if existing_user:
-            flash('Username already exists!', 'danger')
-        else:
-            new_user = User(username=username, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Account created! You can now log in.', 'success')
-            return redirect(url_for('login'))
-    
-    return render_template('register.html')
